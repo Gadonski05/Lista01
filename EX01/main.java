40ec98ea11cba7ac95555600728cbacc742ae442
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-22 21:42:22 -0300

    Reformulação
    Coloque as partes do Main que estavam na variável Calcula.

--- EX01/main.java
+++ EX01/main.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
 
 public class main {
-    
-    public static void main(String[] args) {
         
-        Scanner leitor = new Scanner(System.in);
+    public class calcula {
 
-        System.out.println("---------");
+        public static void main(String[] args) {
+            Scanner Scanner = new Scanner(System.in);
+        
+            System.out.println("-----------------");
+            System.out.println("1 -----SOMA------");
+            System.out.println("2 --SUBTRAÇÃO----");
+            System.out.println("3 -MULTIPLICAÇÃO-");
+            System.out.println("4 ----DIVISÃO----");
+                
+            System.out.println("Escolha o numero da operação: 1 - 2 - 3 - 4 - 5");
+            int operacao = Scanner.nextInt();
 
-        Float soma
-        Float num1 = Float.parseFloat(leitor.nextLine());
-        Float num2 = Float.parseFloat(leitor.nextLine());
+            System.out.println("Primeiro numero: ");
+            float num1 = Scanner.nextFloat();
+
+            System.out.println("Segundo numero: ");
+            float num2 = Scanner.nextFloat();
+
+            float resultado = calcula.calculadora(operacao, num1, num2);
+
+        }
     }
 }
