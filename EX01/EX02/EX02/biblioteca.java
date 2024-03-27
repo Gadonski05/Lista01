6d004c0a6f6164f07046940fb70e6642e1e6a5b6
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-27 16:55:37 -0300

    Chave do final
    O código se manteve inalterado.
    Tirei uma chave do final.

--- EX01/EX02/EX02/biblioteca.java
+++ EX01/EX02/EX02/biblioteca.java
@@ -1,6 +1,28 @@
-public class biblioteca {
+import java.util.ArrayList;
 
-    public static void main(String[] args) {
+// Representação da class principal
+public class biblioteca {
         
+        ArrayList<Livro> biblioteca;
+
+        // Corpo da classe da biblioteca
+        public biblioteca(){
+            // Inicialização da lista
+            biblioteca = new ArrayList<>();
+        }
+
+        public void adclivro(Livro){
+
+            biblioteca.add(Livro);
+        }
+
+        public void mostralivro(){
+
+            System.out.println("Biblioteca");
+
+            for (Livro livro: biblioteca);
+
+            livro.mostralivro();
+            System.out.println();
+        }
     }
-}
