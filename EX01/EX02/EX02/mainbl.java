42608571d6485a6044a81a60a39a506f488e6c52
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-27 17:00:34 -0300

    Face para o usuário
    Implementação da Interface de contato com o usuário.
    Criação do display de opções, com o uso do Switch.
    Depois das opções escolhidas, ele mostra os resultados.

---
+++ EX01/EX02/EX02/mainbl.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class mainbl {
+    public static void main(String[] args) {
+        
+        biblioteca = new biblioteca();
+        Scanner Scanner = new Scanner(System.in);
+
+        System.out.println();
+    }
+}
