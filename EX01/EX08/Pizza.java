f19d7bf8362be7426921405149fc07dbad8271f0
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-28 20:09:34 -0300

    Create Pizza.java

---
+++ EX01/EX08/Pizza.java
@@ -0,0 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Pizza{
+
+    private String nome;
+    private double val;
+    private List<String> comp;
+
+    public Pizza(String nome, double val, List<String> comp){
+
+        this.nome = nome;
+        this.val = val;
+        this.comp = comp;
+    }
+
+    public String getNome(){
+        this.nome;
+
+    }
+
+    public double getVal(){
+        return val;
+
+    }
+
+    public List
+}
