7b797d13fb38da3db311bc0798420ed6e2078c48
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-28 17:32:22 -0300

    Prints
    Ajustes nas mensagens.

--- EX01/EX05/Carro.java
+++ EX01/EX05/Carro.java
@@ -18,14 +18,14 @@ public class Carro {
     // Velocidade atual soma com a velocidade
     public void Velocidade(int velo){
         this.veloAt += velo;
-        System.out.println("Velocidade: " + this.veloAt);
+        System.out.println("Aumentando a Velocidade: " + this.veloAt);
     }
     //Toda velocidade precisa ser maior ou igual a zero
     //Se for menor que zero, está incorreto
     public void Freio(int velo){
         if(this.veloAt - velo >= 0){
             this.veloAt -= velo;
-            System.out.println("Carro está freando: " + this.veloAt);
+            System.out.println("Velocimetro diminuiu para: " + this.veloAt);
 
         }else{
             System.out.println("Carro já está parado!!");
