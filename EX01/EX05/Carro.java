8950bd249df68bf890b556da06be14f223aa6669
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-28 17:02:53 -0300

    Atualização da frenagem
    Colocando o if e else , if caso a velocidade seja maior ou igual a zero.
    Else caso seja diferente.

--- EX01/EX05/Carro.java
+++ EX01/EX05/Carro.java
@@ -15,11 +15,20 @@ public class Carro {
         this.veloAt = 0;
 
     }
+    // Velocidade atual soma com a velocidade
     public void Velocidade(int velo){
         this.veloAt += velo;
         System.out.println("Velocidade: " + this.veloAt);
     }
+    //Toda velocidade precisa ser maior ou igual a zero
+    //Se for menor que zero, está incorreto
     public void Freio(int velo){
-        
+        if(this.veloAt - velo >= 0){
+            this.veloAt -= velo;
+            System.out.println("Carro está freando: " + this.veloAt);
+
+        }else{
+            System.out.println("Carro já está parado!!");
+        }
     }
 }
