8d2a84ba74fa7e81c15d091c3532cc02d005501c
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-28 17:51:52 -0300

    Phone
    Criando e retornando as variáveis.

---
+++ EX01/EX06/Contato.java
@@ -0,0 +1,19 @@
+class Contato{
+
+    private String nome;
+    private int fone;
+
+    public Contato(String nome, int fone){
+        this.nome = nome;
+        this.fone = fone;
+    }
+
+    public String getNome(){
+    return nome;
+
+    }
+    public int getFone(){
+        return fone;
+    }
+
+}
