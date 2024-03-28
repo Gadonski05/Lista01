07cc512cc4b69630dac385a0032659c2c37c4a73
Matheus Henrique Rodrigues Gadonski <142113295+Gadonski05@users.noreply.github.com>
2024-03-28 17:30:42 -0300

    Info do carro
    Mostra todas as informações do carro, até a questão da velocidade.
    inicia do zero e vai aumentando a medida que eu faço a chamada do contador de velocidade.

--- EX01/EX05/MainCarro.java
+++ EX01/EX05/MainCarro.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class MainCarro {
     
+    public static void main(String[] args) {
+        
+        Carro cadaCarro = new Carro("Audi", "RS6", 2015, "GHO012");
+
+        System.out.println("Carro: ");
+        cadaCarro.mostraInfo();
+
+        // Mostra um contador para velocidade
+        cadaCarro.Velocidade(100);
+        cadaCarro.Velocidade(50);
+
+        // Mostra um contador para frenagem
+        cadaCarro.Freio(130);
+
+        System.out.println("Estado do carro: ");
+        cadaCarro.mostraInfo();
+    }
 }
